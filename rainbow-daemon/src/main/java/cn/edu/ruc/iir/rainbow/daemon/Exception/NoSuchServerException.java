19181c226cf625710060ed4b15e9acee0df33aae
Haoqiong Bian <bianhaoqiong@gmail.com>
2018-01-25 23:43:34 +0800

    add daemon

---
+++ rainbow-daemon/src/main/java/cn/edu/ruc/iir/rainbow/daemon/Exception/NoSuchServerException.java
@@ -0,0 +1,5 @@
+package cn.edu.ruc.iir.rainbow.daemon.Exception;
+
+public class NoSuchServerException extends Exception
+{
+}
---
+++ rainbow-daemon/src/main/java/cn/edu/ruc/iir/rainbow/daemon/LayoutServer.java
@@ -0,0 +1,22 @@
+package cn.edu.ruc.iir.rainbow.daemon;
+
+public class LayoutServer implements Server
+{
+    @Override
+    public boolean isRunning()
+    {
+        return Thread.currentThread().isAlive();
+    }
+
+    @Override
+    public void shutdown()
+    {
+
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+}
