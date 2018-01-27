4204f0e1a5e09cd70f8d75e6bfd5c6a56f293c11
Haoqiong Bian <bianhaoqiong@gmail.com>
2018-01-27 17:17:52 +0800

    finish workload

--- rainbow-daemon/src/main/java/cn/edu/ruc/iir/rainbow/daemon/workload/WorkloadServer.java
+++ rainbow-daemon/src/main/java/cn/edu/ruc/iir/rainbow/daemon/workload/WorkloadServer.java
@@ -8,6 +8,9 @@ import cn.edu.ruc.iir.rainbow.daemon.Server;
 import cn.edu.ruc.iir.rainbow.parser.sql.parser.ParsingOptions;
 import cn.edu.ruc.iir.rainbow.parser.sql.parser.SqlParser;
 import cn.edu.ruc.iir.rainbow.parser.sql.tree.*;
+import cn.edu.ruc.iir.rainbow.workload.cache.APCFactory;
+import cn.edu.ruc.iir.rainbow.workload.cache.AccessPattern;
+import cn.edu.ruc.iir.rainbow.workload.cache.AccessPatternCache;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -21,6 +24,20 @@ public class WorkloadServer implements Server
 {
     private boolean shutdown = true;
     private Set<String> processedQueryIds = new HashSet<>();
+    private String tableName;
+    private AccessPatternCache apc;
+
+    /**
+     * The tableName must be full table name like databaseName.tableName
+     * @param tableName
+     * @param lifeTime
+     * @param threshold
+     */
+    public WorkloadServer (String tableName, long lifeTime, double threshold)
+    {
+        this.apc = new AccessPatternCache(lifeTime, threshold);
+        APCFactory.Instance().put(tableName, apc);
+    }
 
     @Override
     public boolean isRunning()
@@ -45,8 +62,6 @@ public class WorkloadServer implements Server
         while (shutdown == false)
         {
             System.out.println("workload is running...");
-            //PrestoQueryPuller puller = new PrestoQueryPuller();
-            //puller.start();
             try {
                 SqlParser parser = new SqlParser();
                 Object obj = HttpUtil.HttpGet(ConfigFactory.Instance().getProperty("presto.query.url"));
@@ -72,20 +87,30 @@ public class WorkloadServer implements Server
 
                                 QuerySpecification queryBody = (QuerySpecification) query.getQueryBody();
                                 // get columns
+                                //TODO: currently we only consider the columns in select statement.
                                 List<SelectItem> selectItemList = queryBody.getSelect().getSelectItems();
 
                                 // tableName
                                 Table table = (Table) queryBody.getFrom().get();
 
-                                System.out.println(table.getName().toString());
+                                if (this.tableName.equalsIgnoreCase(table.getName().toString()))
+                                {
+                                    // this is the query we care about in this pipeline (specified by tableName);
+                                    AccessPattern pattern = new AccessPattern(queryId, 1.0);
+                                    for (SelectItem item : selectItemList)
+                                    {
+                                        //TODO: currently we do not support functions and start(*) in the select statement.
+                                        pattern.addColumn(item.toString());
+                                    }
+                                    this.apc.cache(pattern);
+                                }
                             }
                             this.processedQueryIds.add(queryId);
                         }
                     }
                 }
-
+                // check the queries from presto query url every 5 seconds.
                 TimeUnit.SECONDS.sleep(5);
-
             } catch (InterruptedException e)
             {
                 ExceptionHandler.Instance().log(ExceptionType.ERROR, "error while fetching workload from presto.", e);
--- rainbow-daemon/src/test/java/cn/edu/ruc/iir/daemon/TestPrestoAPI.java
+++ rainbow-daemon/src/test/java/cn/edu/ruc/iir/daemon/TestPrestoAPI.java
@@ -13,11 +13,13 @@ import cn.edu.ruc.iir.rainbow.workload.cache.AccessPatternCache;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class TestPrestoAPI
@@ -25,6 +27,91 @@ public class TestPrestoAPI
     int count = 0;
     private boolean flag = false;
 
+    Map<String, Boolean> queryMap = new HashedMap();
+
+    @Test
+    public void latestTest ()
+    {
+        SqlParser parser = new SqlParser();
+        Query q = null;
+        Object o = new Object();
+        JSONArray jsonArray = null;
+
+        int cou = 0;
+        while (true) {
+            o = HttpUtil.HttpGet(ConfigFactory.Instance().getProperty("presto.query.url"));
+            jsonArray = JSON.parseArray(o.toString());
+
+            String queryId = null;
+            String query = null;
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                if (queryMap.get("queryId") != null && !queryMap.get("queryId") && jsonObject.size() == 8) {
+                    queryId = jsonObject.get("queryId").toString();
+                    queryMap.put("queryId", true);
+                    query = jsonObject.get("query").toString();
+//                    System.out.println(queryId + "\t" + i + "\t" + query);
+                    // Parser
+                    try {
+                        q = (Query) parser.createStatement(query);
+                    } catch (Exception e) {
+                        ExceptionHandler.Instance().log(ExceptionType.ERROR, "query error", e);
+                    }
+//                System.out.println(q.toString());
+                    QuerySpecification queryBody = (QuerySpecification) q.getQueryBody();
+                    // get columns
+                    List<SelectItem> selectItemList = queryBody.getSelect().getSelectItems();
+
+                    // tableName
+                    Table t = (Table) queryBody.getFrom().get();
+//                    System.out.println(t.getName());
+                    if (t.getName().toString().equals("text")) {
+                        System.out.println("Text visit: " + cou++);
+                        if (cou >= 4000) {
+                            System.out.println(queryMap.size());
+                        }
+                        int j = 0;
+                        Random random = new Random(System.currentTimeMillis());
+                        AccessPatternCache APC = new AccessPatternCache(100000, 0.1);
+                        String time = DateUtil.formatTime(new Date());
+                        System.out.println(time);
+                        try {
+                            if (!flag) {
+                                flag = true;
+                                FileUtils.writeFile(time + "\tBegin\t" + i + "\r\n", Settings.APC_PATH, true);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        double weight = Double.parseDouble("1");
+                        AccessPattern pattern = new AccessPattern(queryId, weight);
+                        for (SelectItem column : selectItemList) {
+//                        System.out.println(scolumn.toString());
+                            pattern.addColumn(column.toString());
+                        }
+                        if (APC.cache(pattern)) {
+                            System.out.println(i + ", trigger layout optimization.");
+                            j++;
+                            APC.saveAsWorkloadFile("/home/tao/software/station/Workplace/workload_" + j + ".txt");
+                            try {
+                                flag = false;
+                                System.out.println(time);
+                                FileUtils.writeFile(time + "\tEnd\t" + i + "\r\n", Settings.APC_PATH, true);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        try {
+                            Thread.sleep(random.nextInt(500));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            o = new Object();
+        }
+    }
 
     @Test
     public void HttpGetTest() {
