68abaf27adc4c351fb04f385d12092094390fe45
bian haoqiong <bianhq@ruc.edu.cn>
2018-04-27 22:22:35 +0800

    update test

--- rainbow-layout/src/test/java/cn/edu/ruc/iir/rainbow/layout/TestDMA.java
+++ rainbow-layout/src/test/java/cn/edu/ruc/iir/rainbow/layout/TestDMA.java
@@ -16,42 +16,44 @@ public class TestDMA
         writer.close();
     }
 
-    private static int[] readBatch (BufferedReader reader, int[] a) throws IOException
+    private static String[] readBatch (BufferedReader reader, String[] a) throws IOException
     {
         for (int i = 0; i < 16; ++i)
         {
-            a[0] = Integer.parseInt(reader.readLine());
+            a[i] = reader.readLine();
         }
         return a;
     }
 
-    private static int max(int[] a)
+    private static int max(String[] a)
     {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < a.length; ++i)
         {
-            if (a[i] > max)
+            int ai = Integer.parseInt(a[i]);
+            if (ai > max)
             {
-                max = a[i];
+                max = ai;
             }
         }
         return max;
     }
 
-    private static int min(int[] a)
+    private static int min(String[] a)
     {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < a.length; ++i)
         {
-            if (a[i] < min)
+            int ai = Integer.parseInt(a[i]);
+            if (ai < min)
             {
-                min = a[i];
+                min = ai;
             }
         }
         return min;
     }
 
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, InterruptedException
     {
         //genFile();
 
@@ -81,14 +83,20 @@ public class TestDMA
         System.out.println((System.nanoTime()-start)/1000/1000);
         */
 
+        long start = System.nanoTime();
         Thread thread0 = new Thread(new MyThread("/home/presto/testfile0"));
         Thread thread1 = new Thread(new MyThread("/home/presto/testfile1"));
         Thread thread2 = new Thread(new MyThread("/home/presto/testfile2"));
         Thread thread3 = new Thread(new MyThread("/home/presto/testfile3"));
         thread0.start();
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        //thread1.start();
+        //thread2.start();
+        //thread3.start();
+        thread0.join();
+        //thread1.join();
+        //thread2.join();
+        //thread3.join();
+        System.out.println((System.nanoTime() - start) / 1000 / 1000);
 
     }
 
@@ -107,10 +115,11 @@ public class TestDMA
             try
             {
                 long start = System.nanoTime();
-                int[] a = {4361, 860, 8334, 3310, 8337 - 7929, 6575, -6825, 9373, -110,
-                        -2040, -198, -8604, 1007, -9685, -8613};
+                String[] a = new String[16];
+                        //{4361, 860, 8334, 3310, 8337 - 7929, 6575, -6825, 9373, -110,
+                        //-2040, -198, -8604, 1007, -9685, -8613};
 
-                BufferedReader reader = new BufferedReader(new FileReader(this.file));
+                BufferedReader reader = new BufferedReader(new FileReader(this.file),1024*1024*10);
 
 
                 for (long i = 0; i < 1024 * 1024 * 1024 / 16; i++)
@@ -118,17 +127,21 @@ public class TestDMA
                     readBatch(reader, a);
                 }
 
+
+/*
+                readBatch(reader, a);
+
                 for (long i = 0; i < 1024 * 1024 * 1024 / 16; i++)
                 {
-                    a = readBatch(reader, a);
-                    for (int j = 0; j < 16 * 100; ++j)
+                    //readBatch(reader, a);
+                    for (int j = 0; j < 5; ++j)
                     {
                         int min = min(a);
                         int max = max(a);
                     }
                 }
-
-                System.out.println((System.nanoTime() - start) / 1000 / 1000);
+*/
+                System.out.println(this.file + ": " + (System.nanoTime() - start) / 1000 / 1000);
             }
             catch (Exception e)
             {
