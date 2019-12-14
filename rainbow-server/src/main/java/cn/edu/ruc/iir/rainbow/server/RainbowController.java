5b68303f0559762d0b7128569a2e8362d1731b6c
taoyouxian <taoyouxian@aliyun.com>
2019-12-14 20:36:45 +0800

    update server controller for multiple threads

--- rainbow-server/src/main/java/cn/edu/ruc/iir/rainbow/server/RainbowController.java
+++ rainbow-server/src/main/java/cn/edu/ruc/iir/rainbow/server/RainbowController.java
@@ -16,19 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class RainbowController {
     // daemon
-    private ConcurrentHashMap<String, AtomicLong> daemonLog;
-    private AtomicLong whichId = new AtomicLong(0);
-    private double daemonCost;
+    private ConcurrentHashMap<String, Integer> daemonLog;
+    private AtomicInteger whichLog;
+    private AtomicLong logPosition[];
+    private double daemonCost[];
     private String layout;
 
     // task id
     private String tunePath;
     private String daemonPath;
+    private int daemonNum;
     private String[] curLayout;
     private double lowCost;
 
@@ -36,9 +39,14 @@ public class RainbowController {
     private String default_workload;
     private String default_schema;
 
+    //estimated
+    private Invoker invoker;
+    private Properties params;
+
     @PostConstruct
     private void initSetting() {
         daemonLog = new ConcurrentHashMap<>();
+        whichLog = new AtomicInteger(0);
         tunePath = ConfigFactory.Instance().getProperty("tune.path");
         lowCost = Double.parseDouble(ConfigFactory.Instance().getProperty("tune.cost"));
         curLayout = FileUtils.readFile(tunePath + "tune/column.txt").split(",");
@@ -47,7 +55,20 @@ public class RainbowController {
         default_workload = tunePath + "tune/workload.txt";
         default_schema = tunePath + "tune/schema.txt";
 
-        daemonCost = lowCost;
+        daemonNum = Integer.valueOf(ConfigFactory.Instance().getProperty("tune.daemon.num"));
+        logPosition = new AtomicLong[daemonNum];
+        daemonCost = new double[daemonNum];
+        for (int i = 0; i < daemonNum; i++) {
+            logPosition[i] = new AtomicLong(0);
+            daemonCost[i] = lowCost;
+        }
+
+        // estimated
+        invoker = new InvokerPERFESTIMATION();
+        params = new Properties();
+        params.setProperty("workload.file", default_workload);
+        params.setProperty("num.row.group", "128");
+        params.setProperty("seek.cost.function", "power");
     }
 
     @RequestMapping("/eval")
@@ -83,11 +104,12 @@ public class RainbowController {
     public Response<Object> estimated_seek(@RequestParam("id") String id,
                                            @RequestParam("layout") String layout) {
         double cost;
-        if (!daemonLog.contains(id)) {
-            daemonLog.put(id, whichId);
+        if (!daemonLog.containsKey(id)) {
+            daemonLog.put(id, whichLog.getAndAdd(1));
         }
+        int logIndex = daemonLog.get(id) % daemonNum;
 
-        daemonPath = tunePath + id + "/" + daemonLog.get(id).getAndAdd(1) + "/";
+        daemonPath = tunePath + id + "/" + logPosition[logIndex].getAndAdd(1) + "/";
         FileUtils.mkdir(daemonPath);
 
         if (layout.equals("")) {
@@ -103,23 +125,18 @@ public class RainbowController {
         AnalyzerUtil.getSchemaFromLayout(default_schema, cur_schema, curLayout);
 
         cost = getEstimationTotal(cur_estimated_duration, cur_schema);
-        if (cost != 0 && cost < daemonCost) {
-            System.out.println(id + "-" + (daemonLog.get(id).longValue() - 1) + ">total cost:" + cost);
-            daemonCost = cost;
+        if (cost != 0 && cost < daemonCost[logIndex]) {
+            System.out.println(id + "-" + (logPosition[logIndex].intValue() - 1) + ">total cost:" + cost);
+            daemonCost[logIndex] = cost;
         } else {
             FileUtils.deleteDir(daemonPath);
         }
         return Response.buildSucResp(cost);
     }
 
-    public double getEstimationTotal(String estimated_duration, String cur_schema) {
-        Invoker invoker = new InvokerPERFESTIMATION();
-        Properties params = new Properties();
-        params.setProperty("workload.file", default_workload);
+    private double getEstimationTotal(String estimated_duration, String cur_schema) {
         params.setProperty("schema.file", cur_schema);
         params.setProperty("log.file", estimated_duration);
-        params.setProperty("num.row.group", "128");
-        params.setProperty("seek.cost.function", "power");
         try {
             invoker.executeCommands(params);
         } catch (InvokerException e) {
